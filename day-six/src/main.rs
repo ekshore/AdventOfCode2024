f1243df125afc622b37d4b2bf693d659ffe0e20f
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-12-14 01:23:05 -0600

    Next implimentation

--- day-six/src/main.rs
+++ day-six/src/main.rs
@@ -2,6 +2,13 @@ fn main() {
     println!("Merry Christmas");
 }
 
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
 type Cords = (usize, usize);
 
 struct Guard {
@@ -10,6 +17,7 @@ struct Guard {
     pub data_height: usize,
     actual_width: usize,
     pub pos: Cords,
+    pub dir: Direction,
 }
 
 impl Guard {
@@ -28,20 +36,78 @@ impl Guard {
             .find(|(_, &e)| e == b'^')
             .map_or_else(|| 0, |(idx, _)| idx);
         let pos = (pos_idx % actual_width, pos_idx / actual_width);
+
         let data: Box<[u8]> = raw_data.into();
         Self {
             data,
             data_width,
             data_height,
             actual_width,
+            dir: Direction::Up,
             pos,
         }
     }
 
-    pub fn get_byte(&self, x: usize, y: usize) -> &u8 {
+    pub fn get_position(&self, cords: Cords) -> &u8 {
         self.data
-            .get(y * self.actual_width + x)
+            .get(index(cords, self.actual_width))
             .expect("INDEX OUT OF BOUNDS")
+    }
+
+    pub fn turn(&mut self) {
+        self.dir = match self.dir {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+
+    pub fn next(&mut self) -> Option<usize> {
+        let (x, y) = self.pos;
+        let next = match self.dir {
+            Direction::Up => {
+                if y == 0 {
+                    None
+                } else {
+                    Some((x, y - 1))
+                }
+            }
+            Direction::Down => {
+                if y == self.data_height - 1 {
+                    None
+                } else {
+                    Some((x, y + 1))
+                }
+            }
+            Direction::Right => {
+                if x == self.data_width - 1 {
+                    None
+                } else {
+                    Some((x + 1, y))
+                }
+            }
+            Direction::Left => {
+                if x == 0 {
+                    None
+                } else {
+                    Some((x - 1, y))
+                }
+            }
+        };
+
+        if let Some(next) = next {
+            let next_pos = self.get_position(next);
+            if !(b'.' == *next_pos || b'^' == *next_pos) {
+                self.turn();
+                self.next()
+            } else {
+                self.pos = next;
+                Some(index(next, self.actual_width))
+            }
+        } else {
+            None
+        }
     }
 }
 
@@ -65,7 +131,7 @@ mod day_six {
     #[test]
     fn example_one_data_get_byte_last() {
         let guard = Guard::parse_data(example_data());
-        assert_eq!(b'.', *guard.get_byte(9, 9));
+        assert_eq!(b'.', *guard.get_position((9, 9)));
     }
 
     #[test]
@@ -74,7 +140,28 @@ mod day_six {
         assert_eq!(70, index(guard.pos, guard.actual_width));
     }
 
-    fn example_data() -> Vec<u8>{
+    #[test]
+    fn test_next() {
+        let mut guard = Guard::parse_data(example_data());
+        let next = guard.next();
+        assert!(next.is_some());
+        assert_eq!(59, next.expect("Previously Asserted"));
+    }
+
+    #[test]
+    fn test_next_turn() {
+        let mut guard = Guard::parse_data(example_data());
+        guard.pos = (4, 1);
+        let next = guard.next();
+        assert!(next.is_some());
+        assert_eq!(16, next.expect("Previously Asserted"));
+        guard.pos = (8, 1);
+        let next = guard.next();
+        assert!(next.is_some());
+        assert_eq!(30, next.expect("Previously Asserted"));
+    }
+
+    fn example_data() -> Vec<u8> {
         b"....#.....
 .........#
 ..........
@@ -84,6 +171,7 @@ mod day_six {
 .#..^.....
 ........#.
 #.........
-......#...".into()
+......#..."
+            .into()
     }
 }
