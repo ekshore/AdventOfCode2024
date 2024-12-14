7e6364791bc3e07a70ce2c01ea911362cbb31b3e
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-12-13 20:02:39 -0600

    Start day 3

--- day-three/src/main.rs
+++ day-three/src/main.rs
@@ -1,14 +1,16 @@
+use std::{fs, path::Path};
+
 fn main() {
     println!("Merry Christmas");
 }
 
-struct ReportData {
+struct DataParser {
     data: Box<[u8]>,
     pub cursor: usize,
     data_len: usize,
 }
 
-impl ReportData {
+impl DataParser {
     pub fn new(data: Vec<u8>) -> Self {
         Self {
             data: data.as_slice().into(),
@@ -40,61 +42,72 @@ impl ReportData {
         }
     }
 
-    fn skip_space(&mut self) {
+    fn skip_invalid_chars(&mut self) {
         while let Some(b) = self.peek() {
-            if *b == b' ' {
-                let _ = self.read_byte();
-            } else {
-                break;
-            }
-        }
-    }
-
-    fn read_value(&mut self) -> Option<usize> {
-        let mut buff: Vec<u8> = Vec::new();
-        while let Some(byte) = self.peek() {
-            if *byte == b' ' {
-                self.skip_space();
-                break;
-            }
-            if *byte == b'\n' {
-                break;
-            }
-            buff.push(self.read_byte().unwrap());
-        }
-
-        buff.reverse();
-        buff.into_iter()
-            .enumerate()
-            .map(|(idx, val)| (val - 48) as usize * (10_usize.pow((idx) as u32)))
-            .reduce(|acc, val| (acc + val) as usize)
-    }
-
-    fn read_report(&mut self) -> Option<Vec<usize>> {
-        let mut report: Vec<usize> = Vec::new();
-
-        while let Some(val) = self.read_value() {
-            report.push(val);
-        }
-
-        if let Some(_) = self.peek() {
-            let _ = self.read_byte();
-            Some(report)
-        } else {
-            if report.is_empty() {
-                None
-            } else {
-                Some(report)
+            match *b {
+                b'm' | b'u' | b'l' | b'(' | b')' | b',' | b'0'..=b'9' => {
+                    return;
+                }
+                _ => {
+                    let _ = self.read_byte();
+                }
             }
         }
     }
 }
 
+fn read_file(file_path: String) -> Vec<u8> {
+    let file_path = Path::new(&file_path);
+    let data: Vec<u8> = fs::read(file_path).unwrap();
+    data
+}
+
 #[cfg(test)]
 mod day_three {
-    let example_data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5)";
+    use crate::DataParser;
+
+    fn example_data() -> Vec<u8> {
+        "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5)"
+            .as_bytes()
+            .to_vec()
+    }
 
     #[test]
-    fn example_data() {
+    fn data_parser_peek() {
+        let data = example_data();
+        let report_data = DataParser::new(data);
+        let peek = report_data.peek();
+        assert!(peek.is_some());
+        assert_eq!(b'x', *peek.unwrap());
+        assert_eq!(0, report_data.cursor);
+    }
+
+    #[test]
+    fn data_parser_read_byte() {
+        let data = example_data();
+        let mut report_data = DataParser::new(data);
+        let byte = report_data.read_byte();
+
+        assert!(byte.is_some());
+        assert_eq!(b'x', byte.unwrap());
+        assert_eq!(1, report_data.cursor);
+    }
+
+    #[test]
+    fn data_parser_skip_invalid_chars() {
+        let data = example_data();
+        let mut report_data = DataParser::new(data);
+        report_data.skip_invalid_chars();
+        assert_eq!(1, report_data.cursor);
+        assert_eq!(b'm', *report_data.peek().unwrap());
+
+        report_data.skip_invalid_chars();
+        assert_eq!(1, report_data.cursor);
+        assert_eq!(b'm', *report_data.peek().unwrap());
+
+        report_data.cursor = 9;
+        report_data.skip_invalid_chars();
+        assert_eq!(11, report_data.cursor);
+        assert_eq!(b'm', *report_data.peek().unwrap());
     }
 }
